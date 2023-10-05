73b4749465de3a0cd142f47b2b5736d1b510d573
Ara0n <thomas99.montero@gmail.com>
2023-10-05 12:48:59 +0200

    configs and packages

--- webpack.config.cjs
+++ webpack.config.cjs
@@ -2,108 +2,108 @@
 /* eslint @typescript-eslint/no-var-requires: 0 */
 
 // node imports
-const fs = require('fs')
-const process = require('process')
-const path = require('path')
+const fs = require("fs")
+const process = require("process")
+const path = require("path")
 
 // load all our webpack plugins
-const CopyPlugin = require('copy-webpack-plugin')
-const TerserPlugin = require('terser-webpack-plugin')
-const CssMinimizerPlugin = require('css-minimizer-webpack-plugin')
-const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer')
-const { DefinePlugin } = require('webpack')
-const MiniCssExtractPlugin = require('mini-css-extract-plugin')
-const ReactRefreshWebpackPlugin = require('@pmmmwh/react-refresh-webpack-plugin')
-const ForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin')
-const GenerateFileWebpackPlugin = require('generate-file-webpack-plugin')
+const CopyPlugin = require("copy-webpack-plugin")
+const TerserPlugin = require("terser-webpack-plugin")
+const CssMinimizerPlugin = require("css-minimizer-webpack-plugin")
+const { BundleAnalyzerPlugin } = require("webpack-bundle-analyzer")
+const { DefinePlugin } = require("webpack")
+const MiniCssExtractPlugin = require("mini-css-extract-plugin")
+const ReactRefreshWebpackPlugin = require("@pmmmwh/react-refresh-webpack-plugin")
+const ForkTsCheckerWebpackPlugin = require("fork-ts-checker-webpack-plugin")
+const GenerateFileWebpackPlugin = require("generate-file-webpack-plugin")
 
 // check current env we're building for
-const env = process.env.NODE_ENV ?? 'development'
+const env = process.env.NODE_ENV ?? "development"
 
 // extra options in case we're in specific envs
-let cssLoader = 'style-loader'
+let cssLoader = "style-loader"
 let extraPlugins = []
 let extraBabelPlugins = []
-if (env === 'development') {
+if (env === "development") {
 	// add Fast Refresh if in dev mode
 	extraPlugins.push(new ReactRefreshWebpackPlugin())
-	extraBabelPlugins.push('react-refresh/babel')
+	extraBabelPlugins.push("react-refresh/babel")
 }
-if (env === 'production') {
+if (env === "production") {
 	// extract CSS in prod mode
 	cssLoader = MiniCssExtractPlugin.loader
 	extraPlugins.push(
-		new MiniCssExtractPlugin({ filename: 'assets/code/styles.css' }),
+		new MiniCssExtractPlugin({ filename: "assets/code/styles.css" }),
 	)
 } else {
 	extraPlugins.push(
 		// eslint-disable-next-line @typescript-eslint/ban-ts-comment
 		// @ts-ignore
 		new GenerateFileWebpackPlugin({
-			file: 'assets/code/styles.css',
-			content: '/* using style-loader, no CSS generated */',
+			file: "assets/code/styles.css",
+			content: "/* using style-loader, no CSS generated */",
 		}),
 	)
 }
 
 // setup DefinePlugin with the env itself and the env files
 let defines = {
-	'process.env.NODE_ENV': JSON.stringify(env),
-	'process.env.DEBUG': JSON.stringify(process.env.DEBUG ?? '0'),
+	"process.env.NODE_ENV": JSON.stringify(env),
+	"process.env.DEBUG": JSON.stringify(process.env.DEBUG ?? "0"),
 }
 for (const envFile of [
-	'.env',
-	'.env.local',
+	".env",
+	".env.local",
 	`.env.${env}`,
 	`.env.${env}.local`,
 ]) {
 	let data
 	try {
-		data = fs.readFileSync(envFile, 'utf8')
+		data = fs.readFileSync(envFile, "utf8")
 	} catch (e) {
 		continue
 	}
 	data.split(/\r?\n/g)
-		.map(x => x.split('#')[0])
+		.map(x => x.split("#")[0])
 		.filter(x => x)
-		.map(x => x.split('=', 2))
+		.map(x => x.split("=", 2))
 		.forEach(
 			([k, v]) =>
-				(defines[`process.env.${k}`] = '(' + JSON.stringify(v) + ')'),
+				(defines[`process.env.${k}`] = "(" + JSON.stringify(v) + ")"),
 		)
 }
 
-const sourceRoot = path.normalize(__dirname + '/src')
+const sourceRoot = path.normalize(__dirname + "/src")
 const babelOptions = {
 	presets: [
 		[
-			'@babel/preset-env',
+			"@babel/preset-env",
 			{
 				targets: {
 					esmodules: true,
 				},
 			},
 		],
-		'@babel/preset-typescript',
+		"@babel/preset-typescript",
 		[
-			'@babel/preset-react',
+			"@babel/preset-react",
 			{
-				runtime: 'automatic',
-				development: env === 'development',
+				runtime: "automatic",
+				development: env === "development",
 			},
 		],
 	],
-	sourceType: 'module',
+	sourceType: "module",
 	plugins: [...extraBabelPlugins],
 }
 
 // build our cursed webpack config
 module.exports = [
 	{
-		mode: env === 'production' ? 'production' : 'development',
-		devtool: 'source-map',
+		mode: env === "production" ? "production" : "development",
+		devtool: "source-map",
 		optimization:
-			env === 'production'
+			env === "production"
 				? {
 						minimize: true,
 						minimizer: [
@@ -113,15 +113,15 @@ module.exports = [
 				  }
 				: undefined,
 		resolve: {
-			extensions: ['.js', '.ts', '.tsx'],
+			extensions: [".js", ".ts", ".tsx"],
 		},
-		target: 'web',
+		target: "web",
 		entry: {
-			main: './src/index.tsx',
+			main: "./src/index.tsx",
 		},
 		output: {
-			filename: 'assets/code/index.js',
-			path: __dirname + '/dist',
+			filename: "assets/code/index.js",
+			path: __dirname + "/dist",
 		},
 		module: {
 			rules: [
@@ -130,7 +130,7 @@ module.exports = [
 					include: [sourceRoot],
 					exclude: /node_modules/,
 					use: {
-						loader: 'babel-loader',
+						loader: "babel-loader",
 						options: babelOptions,
 					},
 				},
@@ -141,28 +141,33 @@ module.exports = [
 					use: [
 						cssLoader,
 						{
-							loader: 'css-loader',
+							loader: "css-loader",
 							options: {
 								modules: true,
 								url: {
-									filter: path => !path.startsWith('/'),
+									filter: path => !path.startsWith("/"),
 								},
 							},
 						},
-						'postcss-loader',
-						'sass-loader',
+						"postcss-loader",
+						"sass-loader",
 					],
+				},
+				{
+					test: /\.css$/,
+					include: [__dirname],
+					use: [cssLoader, "css-loader"],
 				},
 			],
 		},
 		plugins: [
 			new BundleAnalyzerPlugin({
-				analyzerMode: 'static',
-				reportFilename: '../report/webpack-analyzer.html',
+				analyzerMode: "static",
+				reportFilename: "../report/webpack-analyzer.html",
 				openAnalyzer: false,
 			}),
 			new CopyPlugin({
-				patterns: [{ from: 'public', to: '.' }],
+				patterns: [{ from: "public", to: "." }],
 			}),
 			new DefinePlugin(defines),
 			new ForkTsCheckerWebpackPlugin({
@@ -171,19 +176,19 @@ module.exports = [
 						semantic: true,
 						syntactic: true,
 					},
-					mode: 'write-references',
+					mode: "write-references",
 				},
 			}),
 			...extraPlugins,
 		],
 		devServer: {
 			static: {
-				directory: __dirname + '/public',
+				directory: __dirname + "/public",
 			},
 			proxy: {
-				'/api': 'http://localhost:3000',
-				'/api/ws': {
-					target: 'ws://localhost:3000',
+				"/api": "http://localhost:3000",
+				"/api/ws": {
+					target: "ws://localhost:3000",
 					ws: true,
 				},
 			},
@@ -191,15 +196,15 @@ module.exports = [
 			hot: true,
 			client: { overlay: false },
 			historyApiFallback: {
-				index: 'index.html',
+				index: "index.html",
 			},
 			setupMiddlewares: (middlewares, devServer) => {
 				if (!devServer) {
-					throw new Error('webpack-dev-server is not defined')
+					throw new Error("webpack-dev-server is not defined")
 				}
-				devServer.app.get('/assets/code/styles.css', (_req, res) => {
-					res.setHeader('content-type', 'text/css')
-					res.send('/* using style-loader, no CSS generated */')
+				devServer.app.get("/assets/code/styles.css", (_req, res) => {
+					res.setHeader("content-type", "text/css")
+					res.send("/* using style-loader, no CSS generated */")
 				})
 				return middlewares
 			},
